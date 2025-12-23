c6be6a8aa2a7ab5f4a7dc817100c688043948f4a
Umer Qureshi <Umerqis21@gmail.com>
2025-12-23 12:09:16 -0500

    tweaked twelvelabs prompt, montages uploaded to bucket

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -23,7 +23,7 @@ export default function Upload() {
 
     let request: MontageRequestDTO = {
       name: `${title}.mp4`,
-      sentence: `Give all time intervals of ${sentence}, only tell me the intervals, nothing else, and in this format: 00:00-00:06, 01:02-01:09, ...`,
+      sentence: `Give all time intervals of ${sentence}, only tell me the intervals, nothing else, and in this format: 00:00-00:06, 01:02-01:09, ... If there are no such time intervals, only return 00:00-00:00`,
       prompt: sentence,
       videoRequestDTOs: videos,
     };
